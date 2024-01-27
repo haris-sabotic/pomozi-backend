0aa2bb99a2e7862060b4cfd8799fadaea8078b16
haris-sabotic <haris.sinfo16@gmail.com>
2024-01-27 21:16:16 +0100

    Fix cors bug

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,20 @@ import bodyParser from "body-parser";
 import * as dotenv from 'dotenv';
 import errorHandlerMiddleware from "./middlewares/errorHandler";
 import router from "./routes";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({
-	limit: '50mb'
+  limit: '50mb'
+}));
+
+app.use(cors({
+  origin: '*', // or specify the domains you want to allow
+  methods: ['GET', 'POST', 'PUT'],
+  allowedHeaders: ['Content-Type'],
 }));
 
 app.use('/api', router);
